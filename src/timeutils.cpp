da212aa95853673afe9e7c6bc6e45d9221afa5c8
powerpack <powerpack@localhost.localdomain>
2020-01-16 14:21:33 -0500

    time utils included

---
+++ src/timeutils.cpp
@@ -0,0 +1,25 @@
+#include "timeutils.h"
+
+// Get time stamp in milliseconds.
+uint64_t millis()
+{
+    uint64_t ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::
+                  now().time_since_epoch()).count();
+    return ms; 
+}
+
+// Get time stamp in microseconds.
+uint64_t micros()
+{
+    uint64_t us = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::
+                  now().time_since_epoch()).count();
+    return us; 
+}
+
+// Get time stamp in nanoseconds.
+uint64_t nanos()
+{
+    uint64_t ns = std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::
+                  now().time_since_epoch()).count();
+    return ns; 
+}
---
+++ src/timeutils.h
@@ -0,0 +1,6 @@
+#include <chrono>
+#include <stdint.h>
+
+uint64_t millis();
+uint64_t micros();
+uint64_t nanos();
