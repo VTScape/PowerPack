f7f61b52d7499757f164667459ac130c8b6bcfcf
Chandler Jearls <cjearls@vt.edu>
2020-01-17 11:32:35 -0500

    Running autoformatter

--- src/serverexample.cpp
+++ src/serverexample.cpp
@@ -1,22 +1,15 @@
 #include "functionapi.h"
 
-void start(){
-   std::cout << "me start\n"; 
+void start() { std::cout << "me start\n"; }
+
+void end() { std::cout << "me end\n"; }
+
+void tag() { std::cout << "me tag\n"; }
+
+int main(int argc, char** argv) {
+  std::string configFile(argv[1]);
+  socketServer server =
+      initializeMeterServer(readServerConfig(configFile), start, end, tag);
+  server.listenForClient();
+  return 0;
 }
-
-void end(){
-std::cout << "me end\n";
-}
-
-void tag(){
-    std::cout << "me tag\n";
-}
-
-
-int main(int argc, char** argv){
-    std::string configFile(argv[1]);
-    socketServer server = initializeMeterServer(readServerConfig(configFile), start, end, tag);
-    server.listenForClient();
-    return 0;
-} 
-
