7cc1ccf09e6862e9d91a9a8de6f5b75bff2fb0b3
sfurman <sfurman@vt.edu>
2020-01-17 09:53:15 -0500

    added ability to read config files for server and client

---
+++ src/functionapi.cpp
@@ -0,0 +1,70 @@
+#include "functionapi.h"
+
+int readServerConfig(std::string configFilePath) {
+  std::string portNumber;
+  std::string tempString;
+  std::ifstream configFile;
+
+  configFile.open(configFilePath);
+  getline(configFile, tempString);
+
+  while (!configFile.eof) {
+    getline(configFile, tempString);
+    if (isSubString(tempString, "port")) {
+      portNumber = extractConfigValue(tempString);
+    } else {
+      std::cerr << "Unrecognized value in server config file\n";
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  if (portNumber == "") {
+    std::cerr << "server portNumber was not set\n";
+    exit(EXIT_FAILURE);
+  }
+
+  return stoi(portNumber, nullptr, 10);
+}
+
+std::pair<int, std::string> readClientConfig(std::string configFilePath) {
+  std::string portNumber = "";
+  std::string IPAddress = "";
+  std::string tempString;
+  std::ifstream configFile;
+
+  configFile.open(configFilePath);
+
+  while (!configFile.eof) {
+    getline(configFile, tempString);
+    if (isSubString(tempString, "port")) {
+      portNumber = extractConfigValue(tempString);
+    } else if (isSubString(tempString, "serveraddress")) {
+      IPAddress = extractConfigValue(tempString);
+    } else {
+      std::cerr << "Unrecognized value in client config file\n";
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  if (portNumber == "" || IPAddress == "") {
+    std::cerr << "portNumber or IPAddress was not set\n";
+    exit(EXIT_FAILURE);
+  }
+
+  return std::make_pair(stoi(portNumber, nullptr, 10), IPAddress);
+}
+
+std::string extractConfigValue(std::string inputString) {
+  size_t delimPosition;
+
+  if ((delimPosition = inputString.find("=")) == std::string::npos) {
+    std::cerr << "Unable to find '=' in config file\n";
+    exit(EXIT_FAILURE);
+  }
+
+  return inputString.substr(delimPosition + 1, std::string::npos);
+}
+
+bool isSubString(std::string inputString, std::string subString) {
+  return inputString.find(subString) != std::string::npos;
+}
--- src/functionapi.h
+++ src/functionapi.h
@@ -1,15 +1,23 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <utility>
 #include "socketutils.h"
 /*
-* API For users who want to generate a power profile for their functions
-* Client and server are two processes
-* Client sends timestamped tags to server
-* Server handles data collection via ni-meter
-*/
+ * API For users who want to generate a power profile for their functions
+ * Client and server are two processes
+ * Client sends timestamped tags to server
+ * Server handles data collection via ni-meter
+ */
 
-void initializeMeterServer();
+socketServer initializeMeterServer(int portNumber);
 void closeMeterServer();
+int readServerConfig(std::string configFilePath);
 
-
-void initializeFunctionClient();
+socketClient initializeFunctionClient(std::pair<int, std::string> clientNetworkInfo);
 void closeFunctionClient();
 void issueTag(std::string tagName);
+std::pair<int, std::string> readClientConfig(std::string configFilePath);
+
+bool isSubString(std::string inputString, std::string subString);
+std::string extractConfigValue(std::string inputString);
